b920ebced64c5f5ff30bf0b8bbb87d3fbd961839
webzinny <webzinny@gmail.com>
2021-07-07 16:28:57 +0530

    ViewsModified

--- TechnoholicBackend/app/urls.py
+++ TechnoholicBackend/app/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns=[
     path('',index,name='index'),
     path('login',login,name='login'),
-    path('signup',signup,name='signup')
+    path('signup',signup,name='signup'),
+    path('course',course,name='course')
 ]
--- TechnoholicBackend/app/views.py
+++ TechnoholicBackend/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Data
+
 
 def index(request):
     return render(request,'index.html')
@@ -8,4 +10,19 @@ def login(request):
     return render(request,'login.html')
 
 def signup(request):
+    if request.method=="POST":
+        email=request.POST['email']
+        pas=request.POST['pas']
+        name=request.POST['name']
+        dob=request.POST['name']
+        gender=request.POST['gender']
+        contact=request.POST['contact']
+        contact2=request.POST['contact2']
+        print("[The DATA is]",end="->")
+        print(email,pas,name,dob,gender,contact,contact2)
+        data=Data(email=email,pas=pas,name=name,dob=dob,gender=gender,contact=contact,contact2=contact2)
+        # data.save()
     return render(request,'signup.html')
+
+def course(request):
+    return render(request,'courseLink.html')
